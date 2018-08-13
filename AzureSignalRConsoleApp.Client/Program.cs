d6af92638fca3287096e8700e2ce8c35f3c0e465
Jeeva Subburaj <vavjeeva@gmail.com>
2018-08-13 12:17:21 -0400

    Initial Version

---
+++ AzureSignalRConsoleApp.Client/Program.cs
@@ -0,0 +1,50 @@
+﻿using AzureSignalRConsoleApp.Utils;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace AzureSignalRConsoleApp.Client
+{
+    class Program
+    {
+        private static readonly string userId = $"User {new Random().Next(1, 99)}";
+        private static ServiceUtils serviceUtils;
+        private static readonly string hubName = "ConsoleAppBroadcaster";
+        private static HubConnection hubConnection;
+
+        async static Task Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddUserSecrets<Program>()
+                .Build();
+
+            serviceUtils = new ServiceUtils(configuration["Azure:SignalR:ConnectionString"]);
+
+            var url = $"{serviceUtils.Endpoint}:5001/client/?hub={hubName}";
+
+            hubConnection = new HubConnectionBuilder()
+                .WithUrl(url, option =>
+                {
+                    option.AccessTokenProvider = () =>
+                    {
+                        return Task.FromResult(serviceUtils.GenerateAccessToken(url, userId));
+                    };
+                }).Build();
+
+            hubConnection.On<string, string>("SendMessage",
+                (string server, string message) =>
+                {
+                    Console.WriteLine($"Message from server {server}: {message}");
+                });
+
+            await hubConnection.StartAsync();
+            Console.WriteLine("Client started... Press any key to close the connection");
+            Console.ReadLine();
+            await hubConnection.DisposeAsync();
+            Console.WriteLine("Client is shutting down...");
+        }
+    }
+}
---
+++ AzureSignalRConsoleApp.Server/Program.cs
@@ -0,0 +1,60 @@
+﻿using AzureSignalRConsoleApp.Utils;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace AzureSignalRConsoleApp.Server
+{
+    class Program
+    {
+        private static readonly HttpClient httpClient = new HttpClient();
+        private static readonly string hubName = "ConsoleAppBroadcaster";
+        private static readonly string serverName = "Azure_SignalR_Server_1";
+        private static ServiceUtils serviceUtils;
+
+        static void Main(string[] args)
+        {
+            //Loading the Configuration Objects from UserSecrets
+            var configuration = new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory())
+               .AddUserSecrets<Program>()
+               .Build();
+
+            serviceUtils = new ServiceUtils(configuration["Azure:SignalR:ConnectionString"]);
+
+            Console.WriteLine(" Azure SignalR Server Started.\n " +
+                              "Start typing and press enter to broadcast messages to all the connected clients.\n " +
+                              "Type quit to shut down the server!");
+            while (true)
+            {
+                var data = Console.ReadLine();
+                if (data.ToLower() == "quit") break;
+                Broadcast(data);
+            }
+
+            Console.WriteLine("SignalR Server is shutting down");
+        }
+
+        private static async void Broadcast(string message)
+        {
+            var url = $"{serviceUtils.Endpoint}:5002/api/v1-preview/hub/{hubName.ToLower()}";
+            var request = new HttpRequestMessage(HttpMethod.Post, new UriBuilder(url).Uri);
+
+            request.Headers.Authorization =
+                new AuthenticationHeaderValue("Bearer", serviceUtils.GenerateAccessToken(url, serverName));
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            request.Content = new StringContent(JsonConvert.SerializeObject(message), Encoding.UTF8, "application/json");
+
+            var response = await httpClient.SendAsync(request);
+            if (response.StatusCode != HttpStatusCode.Accepted)
+            {
+                Console.WriteLine($"Sent error: {response.StatusCode}");
+            }
+        }
+    }
+}
